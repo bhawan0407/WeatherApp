14853e2c6054794ce4f4b81836da1672bb321e6b
Bhawan <bhawandtu93@gmail.com>
2018-07-30 20:21:27 +0530

    Added location as command line argument

--- app.js
+++ app.js
@@ -1,14 +1,36 @@
 const request = require('request');
+const yargs   = require('yargs');
+
+const argv = yargs
+            .options
+            ({
+                a : 
+                {
+                    demand   : true,
+                    describe : 'Address to fetch weather',
+                    alias    : 'address',
+                    string   : true
+                }
+            })
+            .help()
+            .alias('help', 'h')
+            .argv;
+
+// encodeURIComponent
+// decodeURIComponent
+
+var url = `https://maps.googleapis.com/maps/api/geocode/json?address='${encodeURIComponent(argv.address)}`;
 
 var reqOptions = 
 {
-    url  :  "https://maps.googleapis.com/maps/api/geocode/json?address=Sangrur%20Punjab",
+    url,      // url : url
     json : true
 };
 
 request(reqOptions, (error, response, body) => 
 {
     //console.log(JSON.stringify(body, undefined, 2));
-    console.log(`Latitude  ${body.results[0].geometry.location.lat}`);
-    console.log(`Longitude ${body.results[0].geometry.location.lng}`);
+    console.log(`Address   : ${body.results[0].formatted_address}`);
+    console.log(`Latitude  : ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitude : ${body.results[0].geometry.location.lng}`);
 })
